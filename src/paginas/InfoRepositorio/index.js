46e6173bea103db3e5476d4bfcd164c5458b4a6a
victor ciechovicz <victor09ciechovicz@outlook.com>
2022-09-16 00:50:39 -0300

    up

--- src/paginas/InfoRepositorio/index.js
+++ src/paginas/InfoRepositorio/index.js
@@ -1,43 +1,55 @@
-import React, { useState } from 'react';
-import { Text, View, TouchableOpacity, TextInput, Alert } from 'react-native';
+import React, { useState } from 'react'
+import { Text, View, TouchableOpacity, TextInput, Alert } from 'react-native'
 
-import estilos from './estilos';
+import estilos from './estilos'
+
+import {salvarRepositorio} from '../../services/requisicoes/repositorios'
+import { NavigationHelpersContext } from '@react-navigation/native'
 
 export default function InfoRepositorio({ route, navigation }) {
-    const [nome, setNome] = useState('');
-    const [data, setData] = useState('');
+  const [nome, setNome] = useState(route.params.item.name)
+  const [data, setData] = useState(route.params.item.data)
 
+  async function salvar() {
+    const resultado = await salvarRepositorio(
+      route.params.item.postId,
+      nome,
+      data,
+      route.params.item.id
+    )
 
+    if (resultado === 'Sucesso!') {
+      Alert.alert('Repositorio atualizado')
+      navigation.goBack()
+    } else {
+      Alert.alert('Erro ao atualizar repositorio')
+    }
+  }
 
-
-
-
-    return (
-        <View style={estilos.container}>
-            <TextInput
-                placeholder="Nome do repositório"
-                autoCapitalize="none"
-                style={estilos.entrada}
-            />
-            <TextInput
-                placeholder="Data de criação"
-                autoCapitalize="none"
-                style={estilos.entrada}
-            />
-            <TouchableOpacity 
-                style={estilos.botao} 
-            >
-                <Text style={estilos.textoBotao}>
-                    Salvar
-                </Text>
-            </TouchableOpacity>
-            <TouchableOpacity 
-                style={[estilos.botao, {backgroundColor: '#DD2B2B', marginTop: 10}]} 
-            >
-                <Text style={estilos.textoBotao}>
-                    Deletar
-                </Text>
-            </TouchableOpacity>
-        </View>
-    );
+  return (
+    <View style={estilos.container}>
+      <TextInput
+        placeholder="Nome do repositório"
+        autoCapitalize="none"
+        style={estilos.entrada}
+        value={nome}
+        onChangeText={setNome}
+      />
+      <TextInput
+        placeholder="Data de criação"
+        autoCapitalize="none"
+        style={estilos.entrada}
+        value={data}
+        onChangeText={setData}
+      />
+      <TouchableOpacity onPress={salvar} style={estilos.botao}>
+        <Text style={estilos.textoBotao}>Salvar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[estilos.botao, { backgroundColor: '#DD2B2B', marginTop: 10 }]}
+      >
+        <Text style={estilos.textoBotao}>Deletar</Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,8 @@
-import axios from "axios";
-
+import axios from 'axios'
 
 //constante para fazer a conexão com nossa API
-const api= axios.create({
-baseURL:"http://192.168.0.104:3000/"
+const api = axios.create({
+  baseURL: 'http://192.168.0.103:3000/'
 })
 
-export default api;
+export default api
